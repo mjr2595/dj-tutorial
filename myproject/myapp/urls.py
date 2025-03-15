be7528955c06c43d25ee74762b5f4fa92738c060
mjr2595 <michaeljohnraymond@gmail.com>
2025-03-15 12:23:00 -0500

    redo, migration, env

--- myproject/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from . import views
-
-# Define a list of URL patterns
-urlpatterns = [path("", views.index)]
--- worldtour/worldtour/urls.py
+++ worldtour/worldtour/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for worldtour project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("myapp.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
