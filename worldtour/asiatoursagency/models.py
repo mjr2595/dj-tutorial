fd1a1cd166dea70368cc130e2b9302ff58e13683
mjr2595 <michaeljohnraymond@gmail.com>
2025-03-15 13:40:55 -0500

    Add string representation method to Tour model

--- worldtour/asiatoursagency/models.py
+++ worldtour/asiatoursagency/models.py
@@ -7,3 +7,6 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"ID:{self.id} - From {self.origin_country} -> To {self.destination_country} - {self.number_of_nights} nights - ${self.price}"
